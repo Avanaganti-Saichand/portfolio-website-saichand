7aebe68f105e5cc34b4e837e9fa06cb244f9d587
Saichand <saichand98498@gmail.com>
2025-04-10 20:39:52 -0500

    fix: add basePath + assetPrefix for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_ACTIONS;
+
 const nextConfig = {
-    output: "export",
-     images: {
-        unoptimized: true
-    }
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGitHubPages ? "/portfolio-website-saichand" : "",
+  assetPrefix: isGitHubPages ? "/portfolio-website-saichand/" : "",
 };
 
 export default nextConfig;
