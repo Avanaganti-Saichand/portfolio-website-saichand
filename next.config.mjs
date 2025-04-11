29093e0f73e17795adcb4ed5845a78f753996130
Saichand <saichand98498@gmail.com>
2025-04-10 20:45:10 -0500

    fixeed deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isGitHubPages = process.env.GITHUB_ACTIONS;
-
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isGitHubPages ? "/portfolio-website-saichand" : "",
-  assetPrefix: isGitHubPages ? "/portfolio-website-saichand/" : "",
 };
 
 export default nextConfig;
